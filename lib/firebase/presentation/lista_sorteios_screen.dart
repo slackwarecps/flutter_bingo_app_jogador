6bba8da00ea68dadb12d73460768e5c2097c668a
fabiopereiraz <fabio.pereira@icarros.com.br>
2024-07-11 17:56:46 -0300

    feat: Add support for Pod files in Xcode workspace

---
+++ lib/firebase/presentation/lista_sorteios_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class ListaSorteioScreen extends StatefulWidget {
+  const ListaSorteioScreen({super.key});
+
+  @override
+  State<ListaSorteioScreen> createState() => _ListaSorteioScreenState();
+}
+
+class _ListaSorteioScreenState extends State<ListaSorteioScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    // Add your widget tree here
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Lista de Bingos"),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20.0),
+        children: [  
+           
+          Card(
+            child: ListTile(
+              leading: FlutterLogo(size: 72.0),
+              title: Text('Sao Benedito '),
+              subtitle:
+                  Text('2 Cartelas'),
+              trailing: Icon(Icons.more_vert),
+              isThreeLine: true,
+            ),
+          ),
+          
+               Card(
+            child: ListTile(
+              leading: FlutterLogo(size: 72.0),
+              title: Text('Sao Pedro '),
+              subtitle:
+                  Text('0 Cartelas'),
+              trailing: Icon(Icons.more_vert),
+              isThreeLine: true,
+            ),
+          ),
+          
+
+
+               Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on, size: 42,),
+              title: Text('Igreja Santa Isabel '),
+              subtitle:
+                  Text('Comprar Cartelas'),
+              trailing: Icon(Icons.more_vert),
+              isThreeLine: true,
+            ),
+          ),
+
+         
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bingo_jogador/firebase/presentation/home_screen.dart';
+import 'package:bingo_jogador/firebase/presentation/lista_sorteios_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const ListaSorteioScreen(),
     );
   }
 }
