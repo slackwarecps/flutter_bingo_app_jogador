a378299c88dc7d66867b0612ac3aaa771aa5f9ea
FABIO ALVARO PEREIRA <fabio.alvaro@portoseguro.com.br>
2024-07-13 08:06:57 -0300

    roteador de telas

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
-
 import 'package:bingo_jogador/screens/auth_screen.dart';
+import 'package:bingo_jogador/screens/home_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() async{
-
-WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 
-  
-
   FirebaseFirestore firestore = FirebaseFirestore.instance;
-firestore.collection("Só para testar") .doc("Estou testando").set({"funcionou": true,});
+  firestore.collection("Só para testar").doc("Estou testando").set({
+    "funcionou": true,
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -29,11 +29,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
- 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const AuthScreen(),
+      home: const RoteadorTelas(),
     );
   }
 }
@@ -50,19 +49,15 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-  
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-    
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-    
         title: Text(widget.title),
       ),
       body: Center(
@@ -99,6 +94,30 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+//Stream de modificacoes do usuario
+class RoteadorTelas extends StatelessWidget {
+  const RoteadorTelas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.userChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else {
+          if (snapshot.hasData) {
+            //esta logado
+            return HomeScreen();
+          } else {
+            return const AuthScreen();
+          }
+        }
+      },
     );
   }
 }
